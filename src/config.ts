8bba3b88ed8d12f74ce9ae70339521b470238828
mrbublos <skrendel@gmail.com>
2025-06-28 07:43:16 +0300

    s3 client

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,16 @@
-// This file can be used for application-wide configuration.
-export const config = {};
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+    telegram: {
+        token: process.env.TELEGRAM_BOT_TOKEN,
+    },
+    s3: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_KEY,
+        region: process.env.S3_REGION,
+        bucketName: process.env.S3_BUCKET_NAME,
+        endpoint: process.env.S3_ENDPOINT,
+    },
+};
