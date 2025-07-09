e54cd303d8628c3dc5dc67845f1f8f55c5ad7e7d
mrbublos <skrendel@gmail.com>
2025-07-09 15:04:33 +0300

    getdb action

--- buildpush.sh
+++ buildpush.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export VERSION=6
+export VERSION=latest
 docker buildx build --platform linux/amd64 -t skrendelauth/cv_bot:$VERSION -t skrendelauth/cv_bot:latest .
 docker push skrendelauth/cv_bot:$VERSION
